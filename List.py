2dc9ee4098e258af88e20fe62794aac6184b5888
Srirama Joshna <2100032004@kluniversity.in>
2022-08-18 12:05:10 +0530

    First Commit On 18-08-2022 (12:02PM)

---
+++ List.py
@@ -0,0 +1,6 @@
+a=[20,"Hello",9.69]
+print(a)
+print(a[0:3])
+print(a[3:])
+print(a*3)
+print(a+a)
---
+++ String.py
@@ -0,0 +1,11 @@
+a="Hello"
+b="Joshna"
+print(a)
+print(b)
+print(a.upper())
+print(a[2])
+print(b[3])
+print(a[0:3])   #ratio of String a
+print(b[3:])    #slicing of String b
+print(a*3)      #executing the same String 3 times
+print(a+b)      #concatination of String a and String b
